48f3990a0aa195ed29dd2c6b6c19e3a08105c40f
DanielMontoya85 <danielmontoya@syscon.com.co>
2025-10-24 13:46:26 -0500

    cambio en logout

---
+++ Pages/Auth/Logout.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComederoPetWeb.Pages.Auth
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            // Elimina toda la sesión
+            HttpContext.Session.Clear();
+
+            // Evita volver con el botón "Atrás"
+            Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate";
+            Response.Headers["Pragma"] = "no-cache";
+            Response.Headers["Expires"] = "0";
+
+            // Redirige al login
+            return RedirectToPage("/Auth/Login");
+        }
+    }
+}
